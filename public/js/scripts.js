9ed31aa4e54d9d5cb00d579d4f00b95feee92196
Cliente <Cliente@Cliente-PC>
2015-03-03 17:39:11 -0300

    primeiro commit.

---
+++ public/js/scripts.js
@@ -0,0 +1,9 @@
+$(function () {
+    // pegar elemento com corpo da mensagem
+    var corpo_alert = $("#alert-message");
+    
+    // verificar se o elemento está presente na página
+    if (corpo_alert.length)
+        // gerar efeito para o elemento encontrado na página
+            corpo_alert.fadeOut().fadeIn().fadeOut().fadeIn();
+});
